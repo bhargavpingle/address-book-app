58ae9fea421a1fb5be6b45d219319db26af9370c
bhargav <bhargavreddypingle@gmail.com>
2019-12-18 12:00:00 -0600

    Created Main class with options to add, list, search, and delete contacts

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            System.out.println("Address Book Menu:");
+            System.out.println("1. Add Contact");
+            System.out.println("2. List Contacts");
+            System.out.println("3. Search Contact");
+            System.out.println("4. Delete Contact");
+            System.out.println("5. Exit");
+
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();  // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    // Add Contact
+                    System.out.print("Enter name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter phone number: ");
+                    String phoneNumber = scanner.nextLine();
+                    System.out.print("Enter email: ");
+                    String email = scanner.nextLine();
+                    addressBook.addContact(new Contact(name, phoneNumber, email));
+                    System.out.println("Contact added successfully!");
+                    break;
+                case 2:
+                    // List Contacts
+                    System.out.println("Contacts List:");
+                    for (Contact contact : addressBook.getAllContacts()) {
+                        System.out.println(contact);
+                    }
+                    break;
+                case 3:
+                    // Search Contact
+                    System.out.print("Enter name to search: ");
+                    String searchName = scanner.nextLine();
+                    Contact contact = addressBook.findContactByName(searchName);
+                    if (contact != null) {
+                        System.out.println("Contact found: " + contact);
+                    } else {
+                        System.out.println("Contact not found!");
+                    }
+                    break;
+                case 4:
+                    // Delete Contact
+                    System.out.print("Enter name to delete: ");
+                    String deleteName = scanner.nextLine();
+                    if (addressBook.deleteContact(deleteName)) {
+                        System.out.println("Contact deleted successfully!");
+                    } else {
+                        System.out.println("Contact not found!");
+                    }
+                    break;
+                case 5:
+                    // Exit
+                    System.out.println("Goodbye!");
+                    return;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
         }
     }
 }
