aad4dc5fc547cb65783bc7c21fd907c3764156b3
bhargav <bhargavreddypingle@gmail.com>
2019-12-23 12:00:00 -0600

    Added file I/O functionality to save and load contacts

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,10 +7,12 @@ public class AddressBook {
 
     public AddressBook() {
         contacts = new ArrayList<>();
+        loadContacts();
     }
 
     public void addContact(Contact contact) {
         contacts.add(contact);
+        saveContacts();
     }
 
     public List<Contact> getAllContacts() {
@@ -29,8 +32,27 @@ public class AddressBook {
         Contact contact = findContactByName(name);
         if (contact != null) {
             contacts.remove(contact);
+            saveContacts();
             return true;
         }
         return false;
+    }
+
+    private void saveContacts() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("contacts.dat"))) {
+            out.writeObject(contacts);
+        } catch (IOException e) {
+            System.out.println("Error saving contacts: " + e.getMessage());
+        }
+    }
+
+    private void loadContacts() {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("contacts.dat"))) {
+            contacts = (List<Contact>) in.readObject();
+        } catch (FileNotFoundException e) {
+            // No contacts file exists yet, so ignore
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error loading contacts: " + e.getMessage());
+        }
     }
 }
